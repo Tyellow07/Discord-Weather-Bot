2ec7fdb3533b03c9402c22fcc1d9e5778a095bd5
Tyellow07 <the.tyson.huang@gmail.com>
2024-10-13 19:43:06 +0800

    Update restart_bot.sh

--- restart_bot.sh
+++ restart_bot.sh
@@ -1,13 +1,5 @@
 # Discord Weather Bot重新運行腳本
-
-# 激活虛擬環境
 source ~/my-discord-bot/venv/bin/activate
-
-# 停止當前的 bot 進程
 pkill -f bot.py
-
-# 重新運行 bot
 python ~/my-discord-bot/bot.py &
-
-# 退出虛擬環境
 deactivate
