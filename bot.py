184ca32058c21f99be69c71d74f300d76c51247a
Tyellow07 <the.tyson.huang@gmail.com>
2024-10-13 13:44:00 +0800

    dcbot

---
+++ bot.py
@@ -0,0 +1,43 @@
+import os
+import discord
+from discord.ext import commands
+
+# 使用 intents 以確保機器人有權限接收事件
+intents = discord.Intents.default()
+intents.message_content = True  # 啟用讀取訊息內容的權限
+
+# 設置機器人命令的前綴，例如 '!'
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot 已登入為 {bot.user}')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if '早安' in message.content:
+        await message.channel.send('晚安')
+    
+    if '晚安' in message.content:
+        await message.channel.send('不早安')
+    
+    if '午安' in message.content:
+        await message.channel.send('不早不晚安')
+
+    await bot.process_commands(message)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+@bot.command()
+async def 你是誰(ctx):
+    await ctx.send('Hi! 我是用來查詢天氣的機器人')
+
+
+# 使用你的 Bot Token
+bot.run(os.getenv('DISCORD_WEATHER_BOT'))
+
---
+++ restart_bot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 激活虛擬環境
+source ~/my-discord-bot/venv/bin/activate
+
+# 停止當前的 bot 進程
+pkill -f bot.py
+
+# 重新運行 bot
+python ~/my-discord-bot/bot.py &
+
+# 退出虛擬環境
+deactivate
